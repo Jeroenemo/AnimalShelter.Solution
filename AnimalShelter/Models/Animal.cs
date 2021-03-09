d2827af73866bbb9d18d90a709857aa39cc7df6a
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-09 11:36:05 -0800

    add animal model
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,23 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Breed { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    public DateTime DateOfAdmittance { get; set; }
+  }
+}
+
+
+
+// type
+// breed
+// name
+// age
+// gender
+// date_of_admittance
+    
